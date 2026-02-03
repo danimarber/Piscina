d0d4f8fad0bbe580009055455643d6e09339238c
danimarber <danimarber22@gmail.com>
2026-02-03 14:36:16 +0100

    añadido ft_atoi y C05

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	while(nb < 0)
+		return (0);
+
+	while(nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+return (result);	
+}
+/*
+int	main(void)
+{
+	printf("%d", ft_iterative_factorial(5));	
+}*/
